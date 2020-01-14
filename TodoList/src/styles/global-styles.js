725ea087e3731be2b8df2fd3b9007ffa30d409cf
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-01-14 15:10:48 -0300

    header customizado

--- TodoList/src/styles/global-styles.js
+++ TodoList/src/styles/global-styles.js
@@ -5,3 +5,21 @@ export const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
+export const Header = styled.View`
+  position: absolute;
+  top: 0;
+  height: 50px;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border-bottom-color: #2b3dbb;
+  border-bottom-width: 1px;
+  flex-direction: row;
+  padding: 16px;
+`;
+export const HeaderTitle = styled.Text`
+  color: #2b3dbb;
+  font-weight: bold;
+  font-size: 18px;
+`;
