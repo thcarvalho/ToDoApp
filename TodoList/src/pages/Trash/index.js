7551ad1584719baeaf2f931f65e878b66e55cb42
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-01-14 15:09:54 -0300

    tela de tarefas excluidas

---
+++ TodoList/src/pages/Trash/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Header, HeaderTitle } from '../../styles/global-styles';
+import BackButton from '../../components/BackButton';
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
+// import { Container } from './styles';
+
+
+export default function Trash(props) {
+  return (
+    <Header>
+      <TouchableOpacity activeOpacity={0.7} onPress={props.navigation.openDrawer}>
+          <Icon name="bars" size={24} color="#2b3dbb" />
+        </TouchableOpacity>
+      <HeaderTitle>Lixeira</HeaderTitle>
+      <View />
+    </Header>
+  );
+}
