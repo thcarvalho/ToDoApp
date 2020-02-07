5e62ca31bd4138591591ebcc94dbfcee17658b83
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-02-07 11:14:56 -0300

    lixeira funcionando

--- TodoList/src/pages/Trash/index.js
+++ TodoList/src/pages/Trash/index.js
@@ -1,20 +1,76 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import { View, TouchableOpacity, FlatList, ActivityIndicator, Text } from 'react-native';
 import { Header, HeaderTitle } from '../../styles/global-styles';
 import BackButton from '../../components/BackButton';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
-// import { Container } from './styles';
-
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../../services/api';
+import TrashListItem from '../../components/ListItem/TrashListItem';
 
 export default function Trash(props) {
+  const [todos, setTodos] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    recuperarTodos();
+  }, [todos]);
+
+  async function recuperarTodos() {
+    const token = await AsyncStorage.getItem('@userToken');
+    const id = await AsyncStorage.getItem('@userId');
+    try {
+      const response = await api.get(`/todos/${id}/trash`, {
+        headers: { authToken: token },
+      });
+      setTodos(response.data);
+      setRefreshing(false);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+    }
+  }
+
+  function renderItem({ item }) {
+    return (
+      <TrashListItem todo={item} />
+    )
+  }
   return (
-    <Header>
-      <TouchableOpacity activeOpacity={0.7} onPress={props.navigation.openDrawer}>
+    <>
+      {
+        todos != 0 ? (
+          <FlatList
+            style={{ marginTop: 50 }}
+            data={todos}
+            keyExtractor={item => item._id}
+            renderItem={renderItem}
+            onRefresh={() => {
+              recuperarTodos();
+              setRefreshing(true);
+            }}
+            refreshing={refreshing}
+          />
+        ) : (
+            loading ? (
+              <ActivityIndicator animating color={'#2b3dbb'} size={'large'} />
+            ) : (
+                <Text>Nada aqui</Text>
+              )
+          )
+      }
+      <Header>
+        <TouchableOpacity activeOpacity={0.7} onPress={props.navigation.openDrawer}>
           <Icon name="bars" size={24} color="#2b3dbb" />
         </TouchableOpacity>
-      <HeaderTitle>Lixeira</HeaderTitle>
-      <View />
-    </Header>
+        <HeaderTitle>Lixeira</HeaderTitle>
+        <View />
+      </Header>
+    </>
   );
 }
